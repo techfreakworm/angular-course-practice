25ffeca215e4832593d9e3120c0be5c10640461e
Mayank Gupta <techfreakworm@gmail.com>
2019-06-27 14:54:11 +0530

    Use template instead of template URL

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html'
+  template: '<app-server></app-server><app-server></app-server>',
 })
 export class ServersComponent implements OnInit {
 
