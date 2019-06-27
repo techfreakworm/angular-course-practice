0ad13103b55357c004c362d30acaa19429cca77e
Mayank Gupta <techfreakworm@gmail.com>
2019-06-27 14:55:01 +0530

    Multiline tempalte

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  template: `
+  <app-server></app-server>
+  <app-server></app-server>`,
 })
 export class ServersComponent implements OnInit {
 
