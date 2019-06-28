e55c9a2321fc153c6bf93de338df863083ab7776
Mayank Gupta <techfreakworm@gmail.com>
2019-06-28 13:36:40 +0530

    Assignment three, toggle paragraph visibility

---
+++ assignment-three/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { BlockingProxy } from 'blocking-proxy';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'assignment-three';
+  isParagraphVisible = true;
+
+  togglePasswordVisibility(){
+    this.isParagraphVisible = !this.isParagraphVisible;
+  }
+
+  getVisibility(){
+    this.isParagraphVisible? "none": "block";
+  }
+}
