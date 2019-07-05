d66078b4f36bcbe2a7443b56ae71abfd3c4372cc
Mayank Gupta <techfreakworm@gmail.com>
2019-07-05 18:03:39 +0530

    Demonstrate ngOnChanges()

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input,ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +6,20 @@ import { Component, OnInit, Input,ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
   @Input('srvElement') element: {type: string, name: string, content: string};
 
-  constructor() { }
+  constructor() { 
+    console.log('constructor called');
+  }
 
   ngOnInit() {
+    console.log('ng-oninit called');
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges called')
+    console.log(changes)
   }
 
 }
