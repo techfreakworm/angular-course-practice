ed39ac2a36be561e31e9ba6e50c6ee14003f7864
Mayank Gupta <techfreakworm@gmail.com>
2019-07-11 20:03:55 +0530

    Use services for inter component data travel

--- basic-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ basic-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model'
 import { RecipeService } from '../recipe.service';
 
@@ -10,15 +10,11 @@ import { RecipeService } from '../recipe.service';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
 
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe){
-    this.recipeWasSelected.emit(recipe)
-  }
 
 }
--- basic-project/src/app/recipes/recipe.service.ts
+++ basic-project/src/app/recipes/recipe.service.ts
@@ -1,10 +1,13 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core'
 
 export class RecipeService{
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe','This is simple a test', "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOKqRSrKvWvPOq37JYlK2Daz0MLKRbQxZst0cDBbH167mqrAV9"),
         new Recipe('Another Test Recipe','This is simple a test', "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOKqRSrKvWvPOq37JYlK2Daz0MLKRbQxZst0cDBbH167mqrAV9")
       ];
+
+    recipeSelected = new EventEmitter<Recipe>();
 
       getRecipes(){
           return this.recipes.slice();
