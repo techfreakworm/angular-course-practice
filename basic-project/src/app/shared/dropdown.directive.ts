4a4ab582b6b3a0ddad293796cee3c39d4caf6b80
Mayank Gupta <techfreakworm@gmail.com>
2019-07-11 17:17:28 +0530

    Close dropdown when clicking anywhere, not just on the element

--- basic-project/src/app/shared/dropdown.directive.ts
+++ basic-project/src/app/shared/dropdown.directive.ts
@@ -1,12 +1,25 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+// import { Directive, HostListener, HostBinding } from '@angular/core';
 
+// @Directive({
+//     selector: '[appDropdown]'
+// })
+// export class DropdownDirective{
+//     @HostBinding('class.open') isOpen = false;
+
+//     @HostListener('click') toggleOpen(){
+//         this.isOpen = !this.isOpen;
+//     }
+// }
+
+import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
+ 
 @Directive({
-    selector: '[appDropdown]'
+  selector: '[appDropdown]'
 })
-export class DropdownDirective{
-    @HostBinding('class.open') isOpen = false;
-
-    @HostListener('click') toggleOpen(){
-        this.isOpen = !this.isOpen;
-    }
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+  constructor(private elRef: ElementRef) {}
 }
