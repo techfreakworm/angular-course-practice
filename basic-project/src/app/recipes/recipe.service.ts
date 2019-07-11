c7581f13c6b126a1521c4c79ea9554bc97afd384
Mayank Gupta <techfreakworm@gmail.com>
2019-07-11 23:56:43 +0530

    Adding recipe ingredients to shopping list through services

--- basic-project/src/app/recipes/recipe.service.ts
+++ basic-project/src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core'
+import { EventEmitter, Injectable } from '@angular/core'
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService{
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe','This is simple a test', "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOKqRSrKvWvPOq37JYlK2Daz0MLKRbQxZst0cDBbH167mqrAV9", [new Ingredient("Bread", 2), new Ingredient("Flour", 3)]),
@@ -10,7 +12,15 @@ export class RecipeService{
 
     recipeSelected = new EventEmitter<Recipe>();
 
+    constructor(private slService: ShoppingListService){
+
+    }
+
       getRecipes(){
           return this.recipes.slice();
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients);
       }
 }
