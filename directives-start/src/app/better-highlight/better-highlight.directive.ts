415a61f903ad8ecc44d82ad4e9dc3508973ae163
Mayank Gupta <techfreakworm@gmail.com>
2019-07-11 16:29:26 +0530

    Use events to change attributes in in custom attribute directive

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -8,7 +8,15 @@ export class BetterHighlightDirective implements OnInit{
   constructor(private elRef: ElementRef ,private renderer: Renderer2) { }
 
   ngOnInit(){
+    // this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event){
     this.renderer.setStyle(this.elRef.nativeElement,'background-color','blue');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    this.renderer.setStyle(this.elRef.nativeElement,'background-color','transparent');
   }
 
 }
