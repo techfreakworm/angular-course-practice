1cb6693e3338fe102d13b13d312c5b164bec63c9
Mayank Gupta <techfreakworm@gmail.com>
2019-07-18 19:38:27 +0530

    Nested routing complete

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -14,10 +14,8 @@ export class ServerComponent implements OnInit {
   constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id'];
+    const id = +this.route.snapshot.params['id']; // '+' converts a string to number
     this.serverItem = this.serversService.getServer(id);
-    console.log("Got the server: ")
-    console.log(this.serverItem)
     this.route.params.subscribe(
       (params: Params) => {
         this.serverItem = this.serversService.getServer(+params['id']);
