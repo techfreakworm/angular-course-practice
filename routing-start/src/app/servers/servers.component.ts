a76e3cd93adfb325690c4d73fa050fa59139b0f0
Mayank Gupta <techfreakworm@gmail.com>
2019-07-18 16:00:14 +0530

    Navigate to relative routes programmatically

--- routing-start/src/app/servers/servers.component.ts
+++ routing-start/src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,17 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload(){
+    // this.router.navigate(['/servers'])
+    // this.router.navigate(['servers']) // No error here unlike routerLink, typescript doesn't know which component is currently active
+    // this.router.navigate(['servers'], {relativeTo: this.route}) //will give error now, because now it works relative to current path
+    this.router.navigate(['/servers'])
   }
 
 }
