210492d4b9e59cc86a53b74dcdda22c5001f782a
Mayank Gupta <techfreakworm@gmail.com>
2019-07-18 19:53:13 +0530

    Add a '404' component, add it to route with 'something' as routepath'

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent }, 
@@ -21,6 +22,7 @@ const appRoutes: Routes = [
     { path: ':id', component: ServerComponent }, 
     { path: ':id/edit', component: EditServerComponent }, 
   ] }, 
+  { path: 'something', component: PageNotFoundComponent}
 ];
 
 @NgModule({
@@ -31,7 +33,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
