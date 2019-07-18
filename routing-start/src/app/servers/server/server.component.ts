51f2cbebe4466791fe90f3ee1e0706e1eb4a138a
Mayank Gupta <techfreakworm@gmail.com>
2019-07-18 19:26:55 +0530

    Dynamically populate servers by getting id from route

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -9,19 +9,21 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  serverItem: {id: number, name: string, status: string};
 
   constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const id = this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(id);
+    const id = +this.route.snapshot.params['id'];
+    this.serverItem = this.serversService.getServer(id);
+    console.log("Got the server: ")
+    console.log(this.serverItem)
     this.route.params.subscribe(
-      (params:Params) => {
-        this.server = this.serversService.getServer(params['id']);
+      (params: Params) => {
+        this.serverItem = this.serversService.getServer(+params['id']);
       }
     );
-
+    
   }
 
 }
