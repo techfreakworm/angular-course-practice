e1e1fa4642d64d7bf93a02660580348c4f97ff9b
Mayank Gupta <techfreakworm@gmail.com>
2019-07-18 20:21:08 +0530

    Route gaurd working

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGaurd } from './auth-gaurd.service';
 
 
 @NgModule({
@@ -30,7 +32,7 @@ import { AppRoutingModule } from './app-routing.module';
     FormsModule,
     AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGaurd],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
