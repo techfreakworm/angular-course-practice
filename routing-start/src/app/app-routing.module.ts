0e519b6f88038f086fce4acc6b924b86b1690e30
Mayank Gupta <techfreakworm@gmail.com>
2019-07-19 18:01:31 +0530

    removing hash from routes

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -32,7 +32,8 @@ const appRoutes: Routes = [
   
 @NgModule({
     imports:[
-        RouterModule.forRoot(appRoutes, {useHash: true})
+        // RouterModule.forRoot(appRoutes, {useHash: true})
+        RouterModule.forRoot(appRoutes)
     ],
     exports: [
         RouterModule
