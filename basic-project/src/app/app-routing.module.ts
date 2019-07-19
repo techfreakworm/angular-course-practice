8d75dac8748f10c763e8713e4a8965b0cbf682e1
Mayank Gupta <techfreakworm@gmail.com>
2019-07-19 20:03:36 +0530

    Add child routing to see recipe details

--- basic-project/src/app/app-routing.module.ts
+++ basic-project/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { NgModule } from '@angular/core';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    { path: 'recipes', component: RecipesComponent},
+    { path: 'recipes', component: RecipesComponent, children: [
+        { path: '', component: RecipeStartComponent}
+    ]},
     { path: 'shopping-list', component: ShoppingListComponent}
 ];
 
