369b17b70db35236d8fda247fbb2a97e1811077d
Mayank Gupta <techfreakworm@gmail.com>
2019-07-25 19:18:38 +0530

    Pass element reference of HTMLElement on submit of form

--- forms-start/src/app/app.component.ts
+++ forms-start/src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
     const suggestedName = "Superuser";
   }
 
-  onSubmit() {
-    console.log("Submitted!");
+  onSubmit(form: HTMLElement) {
+    console.log(form);
   }
 }
