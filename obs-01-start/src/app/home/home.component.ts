9dc28004675a730d5cb3fdf6a020295352606473
Mayank Gupta <techfreakworm@gmail.com>
2019-07-25 17:15:04 +0530

    Observer error demo

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -19,13 +19,21 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count > 3) {
+          observer.error(new Error("Count is greater than 3"));
+        }
         count++;
       }, 1000);
     });
 
-    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
-      console.log(data);
-    });
+    this.firstObsSubscription = customIntervalObservable.subscribe(
+      data => {
+        console.log(data);
+      },
+      error => {
+        alert(error);
+      }
+    );
   }
 
   ngOnDestroy() {
