1730f5f793d5926c0f395aba582061267a95a743
Mayank Gupta <techfreakworm@gmail.com>
2019-07-25 19:04:47 +0530

    override submit behaviour of angular form

--- forms-start/src/app/app.component.ts
+++ forms-start/src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    const suggestedName = "Superuser";
+  }
+
+  onSubmit() {
+    console.log("Submitted!");
   }
 }
