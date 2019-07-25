6373e94e275ad9fa763cfd4d109fa6a8eef5aa07
Mayank Gupta <techfreakworm@gmail.com>
2019-07-25 17:16:43 +0530

    Complete the observable demo:

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -19,6 +19,9 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count == 2) {
+          observer.complete();
+        }
         if (count > 3) {
           observer.error(new Error("Count is greater than 3"));
         }
@@ -31,7 +34,7 @@ export class HomeComponent implements OnInit, OnDestroy {
         console.log(data);
       },
       error => {
-        alert(error);
+        alert(error.message);
       }
     );
   }
