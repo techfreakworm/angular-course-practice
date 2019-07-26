f900f52a14c2710bba8820ff0c162f031b775565
Mayank Gupta <techfreakworm@gmail.com>
2019-07-27 01:36:42 +0530

    Using types while getting the data in pipe operator

--- http-start/src/app/app.component.ts
+++ http-start/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     this.http
       .post(
@@ -38,19 +39,19 @@ export class AppComponent implements OnInit {
 
   private fetchPosts() {
     this.http.get('https://http-start-2588b.firebaseio.com/posts.json')
-      .pipe(map((responseData) => {
-        const postsArray = [];
+      .pipe(map((responseData: { [key: string]: Post }) => {
+        const postsArray: Post[] = [];
         for (const key in responseData) {
           if (responseData.hasOwnProperty(key)) {
-            postsArray.push({ ...responseData[key] });
+            postsArray.push({ ...responseData[key], id: key });
           }
         }
         return postsArray;
       }
       ))
       .subscribe(
-        responseData => {
-          console.log(responseData);
+        posts => {
+          console.log(posts);
         }
       );
   }
