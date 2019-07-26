fb1488e853244b1316c7086e64ff7711908218bb
Mayank Gupta <techfreakworm@gmail.com>
2019-07-26 17:27:04 +0530

    Clear form

--- basic-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ basic-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -36,7 +36,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
@@ -44,7 +44,13 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.slService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    form.reset();
+  }
 
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
   }
 
 }
