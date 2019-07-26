7ba379cc4c3284281bf50325010113904bd316ba
Mayank Gupta <techfreakworm@gmail.com>
2019-07-27 02:55:03 +0530

    Add Posts service to handle http requests

---
+++ http-start/src/app/posts.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from './post.model';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  createAndStorePost(title: string, content: string) {
+    const postData: Post = { title: title, content: content };
+    this.http
+      .post<{ name: string }>(
+        'https://http-start-2588b.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
+  }
+
+  fetchPosts() {
+    this.http.get<{ [key: string]: Post }>('https://http-start-2588b.firebaseio.com/posts.json')
+      .pipe(map((responseData) => {
+        const postsArray: Post[] = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({ ...responseData[key], id: key });
+          }
+        }
+        return postsArray;
+      }
+      ))
+      .subscribe(
+        posts => { }
+      );
+
+  }
+  constructor(private http: HttpClient) { }
+}
