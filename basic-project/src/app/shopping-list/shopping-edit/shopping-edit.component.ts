e931552324a43825729fb4e356476bde370f0ae3
Mayank Gupta <techfreakworm@gmail.com>
2019-07-26 17:23:16 +0530

    Edit items using form

--- basic-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ basic-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
-import { NgForm } from '@angular/forms';
+import { NgForm, NgFormSelectorWarning } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -10,9 +10,11 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f', { static: false }) slForm: NgForm;
   subscription: Subscription;
   editMode = false;
   edittedItemIndex: number;
+  edittedItem: Ingredient;
 
   constructor(private slService: ShoppingListService) { }
 
@@ -21,6 +23,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       (index: number) => {
         this.edittedItemIndex = index;
         this.editMode = true;
+        this.edittedItem = this.slService.getIngredient(index);
+        this.slForm.setValue({
+          name: this.edittedItem.name,
+          amount: this.edittedItem.amount
+        });
       }
     );
   }
@@ -32,7 +39,12 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   onAddItem(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.slService.addIngredient(newIngredient);
+    if (this.editMode) {
+      this.slService.updateIngredient(this.edittedItemIndex, newIngredient);
+    } else {
+      this.slService.addIngredient(newIngredient);
+    }
+
   }
 
 }
--- basic-project/src/app/shopping-list/shopping-list.service.ts
+++ basic-project/src/app/shopping-list/shopping-list.service.ts
@@ -13,6 +13,10 @@ export class ShoppingListService {
   ];
   constructor() { }
 
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
+
   getIngredients() {
     return this.ingredients.slice();
   }
@@ -24,6 +28,11 @@ export class ShoppingListService {
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
