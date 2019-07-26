6b52d7b1fec3b0987fdea7803ab01b2fea804646
Mayank Gupta <techfreakworm@gmail.com>
2019-07-26 13:43:59 +0530

    Using FormArray in Reactive forms

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -16,11 +16,21 @@ export class AppComponent implements OnInit {
         'username': new FormControl(null, Validators.required),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
-      'gender': new FormControl('male', Validators.required)
+      'gender': new FormControl('male', Validators.required),
+      'hobbies': new FormArray([])
     });
   }
 
   onSubmit() {
     console.log(this.signUpForm);
+  }
+
+  get controls() {
+    return (this.signUpForm.get('hobbies') as FormArray).controls;
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null);
+    (<FormArray>this.signUpForm.get('hobbies')).push(control);
   }
 }
