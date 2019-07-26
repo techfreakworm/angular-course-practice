95048178086df7f0678de64aa36d1d3060b2b8bd
Mayank Gupta <techfreakworm@gmail.com>
2019-07-27 01:20:50 +0530

    Sending http request to firebase API

--- http-start/src/app/app.component.ts
+++ http-start/src/app/app.component.ts
@@ -9,15 +9,15 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     this.http
       .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+        'https://http-start-2588b.firebaseio.com/posts.json',
         postData
       )
       .subscribe(responseData => {
