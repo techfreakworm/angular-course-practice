23ce68d3f569c76a9bb347b8d0cf169d9cbb4279
Mayank Gupta <techfreakworm@gmail.com>
2019-07-27 13:50:39 +0530

    HttpInterceptor basics

---
+++ http-start/src/app/auth-interceptor.service.ts
@@ -0,0 +1,14 @@
+import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+
+export class AuthInterceptorService implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
+        console.log('Request is on its way');
+        const modifiedRequest = req.clone(
+            {
+                url: req.url,
+                headers: req.headers.append('Auth', 'xyz')
+            }
+        );
+        return next.handle(modifiedRequest);
+    }
+}
