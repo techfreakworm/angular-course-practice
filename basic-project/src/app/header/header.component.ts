70d47b7cb1d4f72de26689cdd7dccce9c8e47e97
Mayank Gupta <techfreakworm@gmail.com>
2019-07-27 14:35:12 +0530

    Storing recipes to firebase using HttpClient

--- basic-project/src/app/header/header.component.ts
+++ basic-project/src/app/header/header.component.ts
@@ -1,9 +1,15 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent{
-    
+export class HeaderComponent {
+
+    constructor(private dataStoreService: DataStorageService) { }
+
+    onSaveData() {
+        this.dataStoreService.storeRecipes();
+    }
 }
