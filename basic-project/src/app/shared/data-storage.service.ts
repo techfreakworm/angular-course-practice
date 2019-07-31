6fb4769bce100a7a6fdfcce92f2dd9730240aaa5
Mayank Gupta <techfreakworm@gmail.com>
2019-08-01 00:55:08 +0530

    Add Auth interceptor to add token before sending out the request

--- basic-project/src/app/shared/data-storage.service.ts
+++ basic-project/src/app/shared/data-storage.service.ts
@@ -25,26 +25,21 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.authService.user
-      .pipe(take(1), exhaustMap(user => {
-        return this.http
-          .get<Recipe[]>(
-            'https://ng-course-recipe-book-905bf.firebaseio.com/recipes.json',
-            {
-              params: new HttpParams().set('auth', user.token)
-            }
-          );
-      }), map(
-        recipes => {
-          return recipes.map(recipe => {
-            return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
-          });
-        }
-      ),
+    return this.http
+      .get<Recipe[]>('https://ng-course-recipe-book-905bf.firebaseio.com/recipes.json')
+      .pipe(
+        map(
+          recipes => {
+            return recipes.map(recipe => {
+              return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
+            });
+          }
+        ),
         tap(
           recipes => {
             this.recipeService.setRecipes(recipes);
           }
-        ));
+        )
+      );
   }
 }
