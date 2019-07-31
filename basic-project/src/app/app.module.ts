4c2b58935619f3628b64b5f18c8d5fe5c38e6792
Mayank Gupta <techfreakworm@gmail.com>
2019-08-01 01:27:09 +0530

    Fix linting errors

--- basic-project/src/app/app.module.ts
+++ basic-project/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
--- basic-project/src/app/auth/user.model.ts
+++ basic-project/src/app/auth/user.model.ts
@@ -2,14 +2,14 @@ export class User {
     constructor(
         public email,
         public id: string,
-        private _token: string,
-        private _tokenExpirationDate: Date
+        private TOKEN: string,
+        private TOKEN_EXPIRATION_DATE: Date
     ) { }
 
     get token() {
-        if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
+        if (!this.TOKEN_EXPIRATION_DATE || new Date() > this.TOKEN_EXPIRATION_DATE) {
             return null;
         }
-        return this._token;
+        return this.TOKEN;
     }
 }
